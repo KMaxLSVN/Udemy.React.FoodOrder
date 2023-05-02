89adb1efac3102088cfaeb464f8879cfb351954f
Max Kim <maxim250291@gmail.com>
2023-05-02 17:47:59 +0300

    section 17: adding http & forms

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -46,7 +46,10 @@ const cartReducer = (state, action) => {
     }
     return { items: updateItems, totalAmount: updatedTotalAmount };
   }
-  return defaultCartState; 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
@@ -69,11 +72,18 @@ const CartProvider = (props) => {
     });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartState({
+      type: "CLEAR ",
+    });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
   return (
     <CartContext.Provider value={cartContext}>
